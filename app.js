82b3f30d6a7716fb4c207f01c701d619b5e00481
SolaKabuta <solakabuta@gmail.com>
2023-10-25 16:39:14 +0200

    update-postman-dimierda

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let usrInput = document.querySelector(`input`)
 let search = document.querySelector('#search');
 
+let swiper = document.querySelector(`resultsSwiper`);
+
 // INITIALIZE SEARCH BTN
 let btnSearch = search.querySelector(`button`);
 btnSearch.classList.add(`btn-search`);
@@ -17,14 +19,14 @@ search.addEventListener('keydown', async (event) => {
 
 
 const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzQwMDg4Y2U2YTJiYzVkODAzZGU1ZTIzZGVhYmZhNCIsInN1YiI6IjY1MzI4MTNkNmY4ZDk1MDEyY2QxMDFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GKXXSCmrHAEi0qnltwHnBhFdTBXuLuQVctipXiw0P3M'
-  }
-};
-
-fetch(`https://api.themoviedb.org/3/search/movie?query=${usrInput}&include_adult=false&language=en-US&page=1`, options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzQwMDg4Y2U2YTJiYzVkODAzZGU1ZTIzZGVhYmZhNCIsInN1YiI6IjY1MzI4MTNkNmY4ZDk1MDEyY2QxMDFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GKXXSCmrHAEi0qnltwHnBhFdTBXuLuQVctipXiw0P3M'
+    }
+  };
+  
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${usrInput}&include_adult=false&language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
